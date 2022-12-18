fd4b1a0f9b86c4bf69fbcd4ba8358a0be5d77ace
WooDaeHyun <po668312@naver.com>
2022-12-18 23:59:44 +0900

    feat: React-pagination-sourceCode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Board from "./components/Board";
+import Pagination from "./components/Pagination";
 
 function App() {
+  const [page, setPage] = useState(0);
+
+  const articles = new Array(100).fill().map((_, i) => {
+    return {
+      id: i,
+      title: `${i}번 게시물`,
+    };
+  });
+
+  const limit = 6;
+  const offset = page * limit;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Pagination
+        defaultPage={0}
+        limit={limit}
+        total={articles.length}
+        onChange={setPage}
+      />
+      <Board articles={articles.slice(offset, offset + limit)} />
     </div>
   );
 }
